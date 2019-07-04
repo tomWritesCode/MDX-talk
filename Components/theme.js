3ea7e5645c3fba96d71bce78a41aadca14a1cc39
Thomas Whitaker <thomaswhitaker@protonmail.com>
2019-07-04 14:02:06 +1000

    Asset Inclusion + CSS changes

---
+++ Components/theme.js
@@ -0,0 +1,14 @@
+export default {
+  font: 'dank mono',
+
+  colors: {
+    text: 'ghostwhite',
+    background: '#16161d',
+  },
+
+  Slide: {
+    width: '100vw',
+    height: '100vh',
+    padding: '20px 10%'
+  }
+}
